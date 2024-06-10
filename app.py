bb09a8fde2b7f97c0041ebfa1ae5c36a4920a6bc
23056949github <155886880+23056949github@users.noreply.github.com>
2024-06-11 02:03:58 +0800

    app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ with col1:
 with col2:
     risk_score = st.number_input('Risk Score')
 with col1:
-    location_region = st.selectbox('Transaction Location', ('Africa', 'Asia', 'Europe', 'North America', 'South America'))
+    location_region = st.selectbox(''Weather at Departure Airport - Precipitation', ('None', 'Rain', 'Snow', 'Fog'))
 with col2:
     ip_prefix = st.selectbox('IP Prefix', ('10.0', '172.0', '172.16', '192.0', '192.168'))
 with col1:
